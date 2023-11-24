13ec69d518bef022c90ca815011b3fa095a79bd4
Gabriel Silva <yzgabrieel02@gmail.com>
2023-11-24 00:00:30 -0300

    add folder `config`

---
+++ src/config/image.jsx
@@ -0,0 +1,3 @@
+const image_path = 'https://image.tmdb.org/t/p/w500'
+
+export const ApiImage = image_path
---
+++ src/config/key.jsx
@@ -0,0 +1,3 @@
+const ApiKey1 = "6221c3bb0fd72752d021ca10f5738bba";
+
+export const ApiKey = ApiKey1;
