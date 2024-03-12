3ebcf731de59ea89853facd0124d4995046f607f
Gabriel Silva <yzgabrieel02@gmail.com>
2024-03-12 18:45:12 -0300

    fix issue `search invalid`

---
+++ src/components/NoResults/style.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+    .results {
+        width: 100%;
+        height: 30vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .results h1 {
+        font-size: 250%;
+        color: #000;
+        font-weight: 500;
+    }
+`;
